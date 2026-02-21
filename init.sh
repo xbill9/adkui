74946923f61fa7ee47c3b27ef9abea52f73cad8a
xbill <xbill@glitnir.com>
2026-02-21 16:06:39 -0500

    add support scripts

--- init.sh
+++ init.sh
@@ -1,2 +1,14 @@
 gcloud services enable aiplatform.googleapis.com
 gcloud services enable cloudresourcemanager.googleapis.com
+
+curl -s https://raw.githubusercontent.com/haren-bh/gcpbillingactivate/main/activate.py | python3
+
+cat <<EOF>> .env
+GOOGLE_GENAI_USE_VERTEXAI=1
+GOOGLE_CLOUD_PROJECT=$(gcloud config get-value project)
+GOOGLE_CLOUD_LOCATION=us-central1
+IMAGEN_MODEL="imagen-3.0-generate-002"
+GENAI_MODEL="gemini-2.5-flash"
+EOF
+
+gcloud auth application-default login
