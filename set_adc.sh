3f321e6f836f7fad82a8014dd16b966c4ad4bd56
xbill <xbill9@gmail.com>
2026-03-18 12:15:19 -0400

    better handling

--- set_adc.sh
+++ set_adc.sh
@@ -4,4 +4,11 @@
 # It must be SOURCED to make the variables available in your current shell.
 # Example: source ./set_env.sh
 
-gcloud auth application-default login
+if [ -z "$CLOUD_SHELL" ]; then
+    if ! gcloud auth application-default print-access-token > /dev/null 2>&1; then
+        echo "ADC expired or not found. Initializing login..."
+        gcloud auth application-default login
+    else
+        echo "ADC is valid."
+    fi
+fi
